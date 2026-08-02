9670a6f3907b799cb36d0097a397bdacc0c4ea51
oishiqahmed <oishiqahmed388@gmail.com>
2026-08-02 20:05:15 +0600

    Updated the code

--- Day1/blinking_light.ino
+++ Day1/blinking_light.ino
@@ -4,7 +4,7 @@
 #define ONE_WIRE_BUS 4
 #define LED_PIN 48
 
-const float TEMPERATURE_THRESHOLD_C = 33.0;
+const float TEMPERATURE_THRESHOLD_C = 60.0;
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
@@ -17,7 +17,7 @@ void setup() {
   pinMode(ONE_WIRE_BUS, INPUT_PULLUP);
 
   pinMode(LED_PIN, OUTPUT);
-  neopixelWrite(LED_PIN, 0, 0, 0);
+  neopixelWrite(LED_PIN, 0, 40, 0);
 
   sensors.begin();
   Serial.println("Temperature sensor test");
@@ -32,7 +32,7 @@ void loop() {
   Serial.println(tempC, 1);
 
   if (tempC >= TEMPERATURE_THRESHOLD_C) {
-    Serial.println("[ALERT] TOO HOT! (not really but ok)");
+    Serial.println("[ALERT] TOO HOT!");
     for (int i = 0; i < 3; i++) {
       neopixelWrite(LED_PIN, 100, 0, 100);
       delay(200);
